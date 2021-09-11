10e2263e7f6363bbbf892d4ab45614d0972ad0ce
ezo28 <Abdulaziz.Alhtame@gmail.com>
2021-09-11 23:54:25 +0300

    Build up the sideBar in the index page

--- js/index.js
+++ js/index.js
@@ -3,4 +3,19 @@ $(document).ready(function (){
         $(".nav-item").removeClass("active");
         $(this).addClass("active");
     });
+
+    $("#toggler").click(function(event){
+        $("#wrap").toggleClass('toggled');
+        var right = $(".sidebar").css("right")
+        if (right == "0px")
+        {
+            $(".sidebar").css({"right": "-25rem"});
+            $(".layer").fadeOut();
+        }
+        else{
+            $(".sidebar").css({"right" : "0"});
+            $(".layer").fadeIn();
+        }
+    });
+
 });
