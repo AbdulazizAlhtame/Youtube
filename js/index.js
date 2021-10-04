17a18568d9e8bebaebe08a72fdd92aaa966121ea
ezo28 <Abdulaziz.Alhtame@gmail.com>
2021-10-04 19:50:05 +0300

    Edit watch page

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 $(document).ready(function (){
+    $(".active-element").addClass("active");
+
     $(".nav-item").click(function (){
         $(".nav-item").removeClass("active");
         $(this).addClass("active");
